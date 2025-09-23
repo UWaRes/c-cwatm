81524e4e988549208649baab71ec9f46502e9ab0
Amelie Schmitt <amelie.schmitt@hereon.de>
2025-09-23 11:30:08 +0200

    jobscript

---
+++ jobscript_test_cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=ccwatm                     # Specify job name
+#SBATCH --partition=shared         # Specify partition name for job execution
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:45:00                       # Set a limit on the total run time
+#SBATCH --account=ch0636                     # Charge resources on this project account
+#SBATCH --output=ccwatm.o
+#SBATCH --error=ccwatm.e
+
+ulimit -s unlimited
+module purge
+
+source ~/.bashrc 
+conda activate mpi_env
+
+settingsfile="settings_CCWatM_5min_example.ini"
+
+time srun python3 run_cwatm.py "$settingsfile"
