9e4c9fbe342694d79979330250970be448d7b324
Amelie Schmitt <amelie.schmitt@hereon.de>
2025-11-26 15:21:08 +0100

    calibration tests

--- jobscript_test_cleanup.sh
+++ jobscript_test_cleanup.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=shared         # Specify partition name for job execution
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=00:05:00                       # Set a limit on the total run time
+#SBATCH --time=01:15:00                       # Set a limit on the total run time
 #SBATCH --account=ch0636                     # Charge resources on this project account
 #SBATCH --output=ccwatm.o
 #SBATCH --error=ccwatm.e
@@ -15,6 +15,7 @@ module purge
 source ~/.bashrc 
 conda activate mpi_env
 
-settingsfile="settings_CCWatM_5min_example.ini"
+#settingsfile="settings_CCWatM_5min_example.ini"
+settingsfile="settings_CCWatM_5min_2025_11.ini"
 
 time srun python3 run_cwatm.py "$settingsfile"
